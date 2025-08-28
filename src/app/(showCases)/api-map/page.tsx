ccdb9af12ef465afc480bb1fc566c0069e1e46cb
JihyeonJeong-1117 <wjdwlgus11@ezione.co.kr>
2025-08-28 14:37:23 +0900

    메인화면 api

--- src/app/(showCases)/api-map/page.tsx
+++ src/app/(showCases)/api-map/page.tsx
@@ -3,7 +3,7 @@
 import { Box, Button, Container } from '@mui/material';
 import * as apis from './jsons';
 
-export const API_ROOT = 'http://dev.hifunding.co.kr:30000/api';
+export const API_ROOT = 'http://dev.hifunding.co.kr:30000/api/';
 
 const ApiPage = () => {
   const root = API_ROOT;
@@ -12,12 +12,15 @@ const ApiPage = () => {
     <Container sx={{ pt: 4 }}>
       <Box sx={{ display: 'flex', flexDirection: 'column', gap: 4 }}>
         {list.map(([k, v]) => {
+          const { desc, uri, data } = v;
           return (
             <Box sx={{ p: 2, borderWidth: 1 }} key={k}>
-              <button type="button" onClick={() => console.log(v)}>
-                <h5>{root}</h5>
-                <p>{v.uri}</p>
-                <p>{v.desc}</p>
+              <button type="button" onClick={() => console.log(data)}>
+                <h5>
+                  {root}
+                  {uri}
+                </h5>
+                <p>{desc}</p>
               </button>
             </Box>
           );
